351b16eda98c80879baf35aac861563433f52318
Xiel0112 <guswl000112@gmail.com>
2023-05-17 10:16:41 +0900

    [Footer] 컴포넌트

--- coz-shopping/src/component/Footer.js
+++ coz-shopping/src/component/Footer.js
@@ -1,5 +1,12 @@
+import "../component/Footer.css";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div>개인정보 처리방침 | 이용 약관</div>
+      <div>All rights reserved @ Codestates</div>
+    </div>
+  );
 };
 
 export default Footer;
