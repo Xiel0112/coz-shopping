a9358e00cb6a4b9d0955c5eaca860fb143c78abd
Xiel0112 <guswl000112@gmail.com>
2023-05-18 21:03:52 +0900

    .

--- coz-shopping/src/component/Header.js
+++ coz-shopping/src/component/Header.js
@@ -4,6 +4,7 @@ import product from "../icon/상품 아이콘.png";
 import bookmark from "../icon/북마크 아이콘.png";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import "../component/Header.css";
 
 function Header() {
   const [dropdown, setDropdown] = useState(false);
@@ -12,33 +13,29 @@ function Header() {
 
   const Dropdown = () => {
     return (
-      <div>
-        <div>○○○님, 안녕하세요!</div>
-        <div>
-          <Link to={"/products/list"}>
-            <img src={product} alt="상품" />
-            <div> 상품 리스트</div>
-          </Link>
-          <Link to={"/products/list"}>
-            <img src={bookmark} alt="북마크" />
-            <div> 북마크</div>
-          </Link>
-        </div>
+      <div className="modal">
+        <div className="modal-welcome">○○○님, 안녕하세요!</div>
+        <Link className="modal__productList" to={"/products/list"}>
+          <img className="productList-icon" src={product} alt="상품" />
+          <div className="productList-text">상품 리스트</div>
+        </Link>
+        <Link className="modal__bookmark" to={"/products/list"}>
+          <img className="bookmark-icon" src={bookmark} alt="북마크" />
+          <div className="bookmark-text">북마크</div>
+        </Link>
       </div>
     );
   };
 
   return (
     <div className="header">
-      <div>
-        <Link to={"/"}>
-          <img src={logo} alt="로고" />
-        </Link>
-        <Link to={"/"}>
-          <div>COZ Shopping</div>
-        </Link>
-      </div>
-      <img src={menu} alt="메뉴" onClick={handleDropDown} />
+      <Link className="header__logo" to={"/"}>
+        <img src={logo} alt="로고" />
+      </Link>
+      <Link className="header_storeName" to={"/"}>
+        <div>COZ Shopping</div>
+      </Link>
+      <img className="header__menu" src={menu} alt="메뉴" onClick={handleDropDown} />
       {dropdown && <Dropdown />}
     </div>
   );
