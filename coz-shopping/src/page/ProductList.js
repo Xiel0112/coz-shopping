fe5d2c5e9066ffc32f9fd64cc84352f6e7d0d587
Xiel0112 <guswl000112@gmail.com>
2023-05-18 12:37:00 +0900

    .

--- coz-shopping/src/page/ProductList.js
+++ coz-shopping/src/page/ProductList.js
@@ -1,9 +1,17 @@
+import { useContext, useState } from "react";
 import Classification from "../component/Classification";
 import Footer from "../component/Footer";
 import Header from "../component/Header";
 import Product from "../component/Product";
+import { ProuductsContext } from "../context/ProductsProvider";
+import Modal from "../component/Modal";
 
 function ProductList() {
+  const products = useContext(ProuductsContext);
+  const [modal, setModal] = useState(false);
+
+  const handleOpenModal = () => setModal(true);
+
   return (
     <div>
       <Header />
@@ -11,9 +19,23 @@ function ProductList() {
         <div>
           <Classification />
         </div>
-        <div>
-          <Product />
+        <div onClick={handleOpenModal}>
+          {products.map((product, id) => (
+            <Product
+              key={id}
+              type={product.type}
+              title={product.title}
+              sub_title={product.sub_title}
+              brand_name={product.brand_name}
+              price={product.price}
+              discountPercentage={product.discountPercentage}
+              image_url={product.image_url}
+              brand_image_url={product.brand_image_url}
+              follower={product.follower}
+            />
+          ))}
         </div>
+        {modal && <Modal setModal={setModal} />}
       </main>
       <Footer />
     </div>
