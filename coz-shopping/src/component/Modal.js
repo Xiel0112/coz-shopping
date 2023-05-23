09d87253b34dca97de0fb7e5d38303457b55bd15
Xiel0112 <guswl000112@gmail.com>
2023-05-23 15:27:22 +0900

    .

--- coz-shopping/src/component/Modal.js
+++ coz-shopping/src/component/Modal.js
@@ -1,12 +1,15 @@
+import "./Modal.css";
+import Vector from "../icon/Vector.png";
+
 function Modal({ setModal, img }) {
   const handleCloseModal = () => {
     setModal(false);
   };
 
   return (
-    <div>
-      <img src={img} alt="상품" />
-      <img src="!!:" alt="닫기" onClick={handleCloseModal} />
+    <div className="modal">
+      <img className="modal__img" src={img} alt="상품" />
+      <img className="modal__close" src={Vector} alt="닫기" onClick={handleCloseModal} />
     </div>
   );
 }
