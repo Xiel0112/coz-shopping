c1cda276ebb5d094976cd25d1a043a4c6a6ad307
Xiel0112 <guswl000112@gmail.com>
2023-05-23 18:05:17 +0900

    .

--- coz-shopping/src/component/FilterBtn.js
+++ coz-shopping/src/component/FilterBtn.js
@@ -6,7 +6,7 @@ function FilterBtn({ type, title, setSelectedType, image }) {
   return (
     <div className="type-btn" onClick={handleselectedType}>
       <img className="type-btn-img" src="!!:" alt="카테고리" />
-      <div>{title}</div>
+      <div className="type-btn-text">{title}</div>
     </div>
   );
 }
